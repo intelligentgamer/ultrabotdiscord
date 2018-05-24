ddc66a09a4c0950d4f3edc7d7d97597adcf62fac
intelligentgamer <39501953+intelligentgamer@users.noreply.github.com>
2018-05-24 18:01:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require ("discord.js")
 const client = new Discord.Client()
 var prefix = "ub!!";
-client.login("NDQ4NTc5NDQwMTU5NzUyMTky.DeYi2A.5uiKl7I5ZwkdfmqYNzYoVP7ZX6s")
+client.login(process.env.TOKEN)
 
 client.on("ready", function() {
     client.user.setActivity("ub!!", {type:"PLAYING"})
