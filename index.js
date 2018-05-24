1b6cea2d36e1c1fb0ec1e48eb59d434f3e599c1f
intelligentgamer <39501953+intelligentgamer@users.noreply.github.com>
2018-05-24 18:46:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const client = new Discord.Client()
 var prefix = "ub!!";
 client.login(process.env.TOKEN)
 
+client.on(`message`, message => {
+    
+    if(message.content === "Bonjour"){
+        message.reply("salut je m'appelle ultrabot");
+        console.log("Le bot dit bonjour");
+    }
+});
+
 client.on("ready", function() {
     client.user.setActivity("ub!!", {type:"PLAYING"})
     console.log("Bot Ready")
