ad055f40df9768fa9cae37165c24d11451c970e0
intelligentgamer <39501953+intelligentgamer@users.noreply.github.com>
2018-05-25 23:30:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,11 @@ client.on(`message`, message => {
     if(message.content === "Bonjour tu fait quoi"){
         message.reply("salut je m'appelle ultrabot et Je te surveille XDDD");
         console.log("Le bot dit bonjour et Je te surveille XDDD");
-    }
-});
-client.on('message', message => {
-    if(message.content === "ub!!Ping"){
-       message.reply("pong :ping_pong: ");
-       console.log('Le bot dit Pong !');
-    }       
-})
+    if(message.content === "ub!!Ping")
+       message.reply("pong :ping_pong: ")
+       console.log('Le bot dit pong !') 
+        }
+    });
 client.on("ready", function() {
     client.user.setActivity("ub!!|cree par Jean0", {type:"PLAYING"})
     console.log("Bot Ready")
